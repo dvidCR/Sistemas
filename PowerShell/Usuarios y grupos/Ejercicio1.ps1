8407eca04b9492d5a218576747f8094c061e6bd7
dvidCR <dvidcr100@gmail.com>
2024-02-15 19:27:12 +0100

    2 Commit

---
+++ PowerShell/Usuarios y grupos/Ejercicio1.ps1
@@ -0,0 +1,26 @@
+﻿function crear($usuario, $contraseña) {
+    New-LocalUser $usuario -Password(ConvertTo-SecureString $usuario -AsPlainText -Force) -FullName $usuario -Description 'Usuario número 2'
+
+}
+
+crear "tarea" "Hola#1234"
+
+function borrar($usuario) {
+    Remove-LocalUser $usuario
+}
+
+borrar "tarea"
+
+
+function leer($fichero) {                                                                    
+    # Añadir el fichero a una variable                                                                
+    $contenido = Get-Content $fichero
+                                                              
+    # Leer cada línea del fichero y crear otro fichero con el número línea y el contenido             
+    for ($i=0 ; $i -lt $contenido.Length ; $i++){
+        [String]$i+" "+$contenido[$i]
+        New-LocalUser $contenido[$i] -Password(ConvertTo-SecureString "Hola#1234" -AsPlainText -Force) -FullName $contenido[$i] 
+      }
+}
+
+leer "D:\Clase\Sistemas\PowerShell\Usuarios y grupos\ejAvanzado.txt"
---
+++ PowerShell/Usuarios y grupos/Gestion de usuarios y grupos.ps1
@@ -0,0 +1,30 @@
+﻿Get-Command -Module Microsoft.Powershell.LocalAccounts
+
+Get-LocalUser
+
+Get-LocalUser | Format-Table Name,Enabled
+
+Get-LocalUser -Name Administrador
+
+#Crear
+#$password = Read-Host -AsSecureString
+#New-LocalUser 'Usuario2' -Password $password -FullName 'Usuario 2 curso' -Description 'Usuario número 2'
+
+#Borrar
+#Remove-LocalUser Usuario1
+#Get-LocalUser | Format-Table Name,Enabled
+
+#Modificar
+#Rename-LocalUser -Name 'Usuario2' -Newname 'lagomaruser'
+#Set-LocalUser -Name 'lagomaruser' -Description 'Responsable TIC'
+#Get-LocalUser -Name 'lagomaruser' | Set-Localuser -FullName  'Pepe'
+#Set-LocalUser -Name 'lagomaruser' –PasswordNeverExpires $False
+
+#Añadir a un grupo
+#Get-Localuser -Name 'lagomaruser' | Add-LocalGroupMember -Group 'Usuarios'
+#Enable-User -Name 'lagomaruser'
+
+#Modificar contraseña
+#$password = Read-Host -AsSecureString
+#$usuario = Get-LocalUser -Name 'lagomaruser'
+#$usuario | Set-LocalUser -Password $password
